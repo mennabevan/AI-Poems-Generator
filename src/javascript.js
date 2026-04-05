a8cfdaf7358fa464a37c713eb61445715a419037
mennabevan <menna.bevan@outlook.com>
2026-04-05 22:07:51 +0100

    Added axios, API key, url and AI prompt and context

--- src/javascript.js
+++ src/javascript.js
@@ -1,13 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Poem Hereeeeee",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     cursor: "",
   });
 }
 
-let poemForElement = document.querySelector("#poem-generator-form");
-poemForElement.addEventListener(`submit`, generatePoem);
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "6bct4e17oa2f4e400a14bd864136d360";
+  let prompt = `Generate a poem about ${instructionsInput.value}`;
+  let context =
+    "You are a funny peom expert, wanting to put together short poem. Your mission put together a basic 4 line poem, seperate each line. Make sure to follow the user instructions which will be inputted.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener(`submit`, generatePoem);
